e25e688e9c3dc9f31c31ff73caff868c83b26854
Lauramarie Laskey <ll00098@my.westga.edu>
2022-08-26 19:32:35 -0400

    Checkstyle resolutions

--- babble/src/test/java/edu/westga/cs/babble/controllers/TestWordDictionary.java
+++ babble/src/test/java/edu/westga/cs/babble/controllers/TestWordDictionary.java
@@ -1,36 +1,61 @@
 package edu.westga.cs.babble.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import edu.westga.cs.babble.controllers.WordDictionary;
-
+/**
+ * Test class for WordDictionary
+ * 
+ * @author lewisb
+ * @version 6241
+ *
+ */
 public class TestWordDictionary {
 
-	WordDictionary dictionary;
+	private WordDictionary dictionary;
 	
+	/**
+	 * Method that sets up a new WordDictionary
+	 * 
+	 * @throws Exception
+	 */
 	@BeforeEach
 	public void setUp() throws Exception {
 		this.dictionary = new WordDictionary();
 	}
 	
+	/**
+	 * Tests the validity of string 'expand'
+	 */
 	@Test
 	public void stringExpandShouldBeValid() {
 		assertTrue(this.dictionary.isValidWord("Expand"));
 	}
 	
+	/**
+	 * Tests the validity of string 'bugblat'
+	 */
 	@Test
 	public void stringBugblatShouldNotBeValid() {
 		assertFalse(this.dictionary.isValidWord("Bugblat"));
 	}
 	
+	/**
+	 * Tests the validity of an empty string
+	 * 
+	 */
 	@Test
 	public void emptyStringShouldNotBeValid() {
 		assertFalse(this.dictionary.isValidWord(""));
 	}
 
+	/**
+	 * Tests a null value for a word
+	 * 
+	 */
 	@Test
 	public void shouldNotAcceptNull() {
 		assertFalse(this.dictionary.isValidWord(null));
--- babble/src/test/java/edu/westga/cs/babble/model/TestTileBagDrawTile.java
+++ babble/src/test/java/edu/westga/cs/babble/model/TestTileBagDrawTile.java
@@ -1,6 +1,5 @@
 package edu.westga.cs.babble.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
